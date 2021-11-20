4b3919401fb45003ac1f94275084434906cf8975
DENRIG777 <62824680+DENRIG777@users.noreply.github.com>
2021-11-20 01:06:01 -0600

    added content

--- js/site.js
+++ js/site.js
@@ -18,11 +18,6 @@ function reverseString(userString){
     let revString = [];
 
 
-    // let name=Grace
-    // name[0]=G
-    // name[4]=e
-    // last pos in an array name.length -1
-
     //reverse a string using a for loop
     for (let index = userString.length - 1 ; index >=  0; index--) {
         revString += userString[index];
